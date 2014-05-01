9a622c42e8800610fb568497b47922d648d70adc
Brandon Wagner <brandon@tripletech.net>
2014-04-30 23:25:48 -0400

    added more client test data to populate larger list views

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,6 +5,9 @@ class BootStrap {
 
     def init = { servletContext ->
         Client client = new Client(name: "First Energy Nuclear Operating Company").save()
+        Client client2 = new Client(name: "Triple Tech, Inc.").save()
+        Client client3 = new Client(name: "United States Department of Agriculture").save()
+        Client client4 = new Client(name: "Nebraska Public Power District").save()
         Product product = new Product(name: "FPE", description: "FPE", client: client).save()
     }
     def destroy = {
