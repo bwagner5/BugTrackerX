33553aff663a7c89be40aebaafe91633ae82f76b
Brandon Wagner <brandon@tripletech.net>
2014-05-01 22:57:51 -0400

    added some URL mapping to create bug page and bootstrapped some more objects for testing

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,6 @@
 import com.btx.Client
 import com.btx.Product
+import com.btx.Severity
 
 class BootStrap {
 
@@ -8,7 +9,18 @@ class BootStrap {
         Client client2 = new Client(name: "Triple Tech, Inc.").save()
         Client client3 = new Client(name: "United States Department of Agriculture").save()
         Client client4 = new Client(name: "Nebraska Public Power District").save()
-        Product product = new Product(name: "FPE", description: "FPE", client: client).save()
+        Product product = new Product(name: "Fire Protection Expert : Davis-Besse", description: "Davis-Besse's Version of Triple Tech's Fire Protection Expert software.", client: client).save()
+        Product product2 = new Product(name: "Fire Protection Expert : Perry", description: "Perry's Version of Triple Tech's Fire Protection Expert software.", client: client).save()
+        Product product3 = new Product(name: "Fire Protection Expert : Beaver Valley Unit 1", description: "Beaver Valley Unit 1's Version of Triple Tech's Fire Protection Expert software.", client: client).save()
+        Product product4 = new Product(name: "Fire Protection Expert : Beaver Valley Unit 2", description: "Beaver Valley Unit 2's Version of Triple Tech's Fire Protection Expert software.", client: client).save()
+        Product product5 = new Product(name: "Bug Tracker X", description: "Bug Tracker X is a web application to easily and quickly track and resolve bugs. Bug Tracker X gives the client the ability to report bugs and be notified when they have been resolved.", client: client2).save()
+
+        Severity info = new Severity(name: "Informational", level: 0).save()
+        Severity minor = new Severity(name: "Minor", level: 1).save()
+        Severity moderate = new Severity(name: "Moderate", level: 2).save()
+        Severity major= new Severity(name: "Major", level: 3).save()
+        Severity critical = new Severity(name: "Critical", level: 4).save()
+
     }
     def destroy = {
     }
