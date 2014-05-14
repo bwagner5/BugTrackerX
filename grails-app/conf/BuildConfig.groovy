b6c40f469dfab7acb45a79c52260d5ef1cfb2634
Brandon Wagner <bmwagner10@gmail.com>
2014-05-14 15:04:20 -0400

    works as before upgrade to 2.4.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -69,7 +69,6 @@ grails.project.dependency.resolution = {
         runtime ":hibernate4:4.3.5.2" // or ":hibernate:3.6.10.14"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
-    	runtime ":twitter-bootstrap:3.1.1"
 
 
         // Uncomment these to enable additional asset-pipeline capabilities
